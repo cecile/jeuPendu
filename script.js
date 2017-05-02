7c5c4c85dbe02d90dd2bb5d4ebb1375c00ffbd9e
cecile <cec.jobert@gmail.com>
2017-05-02 13:41:32 +0200

    css/js

---
+++ script.js
@@ -0,0 +1,49 @@
+var motMystere;
+//var motAtrouver = new Array();
+var mots = new Array("javascript", "bonjour", "decouverte", "youpi", "code", "lexomil", "desespoir", "absurde", "fastidieux", "malchanceux");
+
+
+var motMystere = mots[math.floor(math.random()*10)];
+var nombrecoup = 7;
+var coupjouer = 0; 
+
+var a = document.querySelector("a");
+var b = document.querySelector("b");
+var c = document.querySelector("c");
+var d = document.querySelector("d");
+var e = document.querySelector("e");
+var f = document.querySelector("f");
+var g = document.querySelector("g");
+var h = document.querySelector("h");
+var i = document.querySelector("i");
+var j = document.querySelector("j");
+var k = document.querySelector("k");
+var l = document.querySelector("l");
+var m = document.querySelector("m");
+var n = document.querySelector("n");
+var o = document.querySelector("o");
+var p = document.querySelector("p");
+var q = document.querySelector("q");
+var r = document.querySelector("r");
+var s = document.querySelector("s");
+var t = document.querySelector("t");
+var u = document.querySelector("u");
+var v = document.querySelector("v");
+var w = document.querySelector("w");
+var x = document.querySelector("x");
+var y = document.querySelector("y");
+var z = document.querySelector("z");
+
+
+
+
+a.addEventListener("click",verifier);
+b.addEventListener("click",verifier);
+c.addEventListener("click",verifier);
+d.addEventListener("click",verifier);
+e.addEventListener("click",verifier);
+f.addEventListener("click",verifier);
+g.addEventListener("click",verifier);
+h.addEventListener("click",verifier);
+i.addEventListener("click",verifier);
+j.addEventListener("click",verifier);
